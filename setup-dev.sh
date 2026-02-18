d2ca8f9b5af1ce22b6b9bbcee315119f09994174
Scott Stevenson <scott@stevenson.io>
2026-02-18 20:46:53 +0000

    Install additional dev tools

--- setup-dev.sh
+++ setup-dev.sh
@@ -11,7 +11,8 @@ pacman_syu() {
 }
 
 info "Installing development tools..."
-pacman_syu difftastic fd fzy git helix jujutsu neovim ripgrep starship tmux
+pacman_syu chezmoi difftastic fd fzy git helix jujutsu neovim ripgrep \
+  starship tmux typos-lsp
 sudo ln -fs /usr/bin/helix /usr/local/bin/hx
 
 info "Installing TOML tools..."
@@ -22,6 +23,9 @@ pacman_syu prettier
 
 info "Installing shell tools..."
 pacman_syu bash-language-server shellcheck shfmt
+
+info "Installing Lua tools..."
+pacman_syu lua-language-server stylua
 
 info "Installing Python tools..."
 pacman_syu ruff ty
